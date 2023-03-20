c848daa9dbe17e782c037f62b1a2a2fd5ce932d5
hazemghaly <hazemghaly30@gmail.com>
2023-03-19 19:09:05 -0700

    task 06

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+/*declare a variable*/
+char alphabet;
+for (alphabet = 'z'; alphabet <= 'z' ; alphabet--)
+{
+/* print the value of the variable*/
+putchar(alphabet);
+}
+putchar('\n');
+return (0);
+}
