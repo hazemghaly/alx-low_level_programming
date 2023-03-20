5c5d2c73c0a7783913fc2f8de8947957e40b8412
hazemghaly <hazemghaly30@gmail.com>
2023-03-19 19:20:27 -0700

    task 08

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+int n;
+char al
+for (n = 0; n < 10; n++)
+{
+	putchar(n);
+}
+for (al = 'a' ; al <= 'f'; al++)
+{
+        putchar(al);
+}
+putchar('\n');
+return (0);
+}
