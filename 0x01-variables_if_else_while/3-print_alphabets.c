5be9378cee3cee06992b18ac0c919cfb6a51d885
hazemghaly <hazemghaly30@gmail.com>
2023-03-19 18:18:17 -0700

    Task 03

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,12 +8,13 @@ int main(void)
 {
 /*declare a variable*/
 char alphabet;
-for (alphabet = 'a'; alphabet <= 'z' ; alphabet++){
+for (alphabet = 'A'; alphabet <= 'Z' ; alphabet++){
 for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
 {
 /* print the value of the variable*/ 
 putchar(alphabet);
-}}
+}
+}
 putchar('\n');
 return (0);
 }
