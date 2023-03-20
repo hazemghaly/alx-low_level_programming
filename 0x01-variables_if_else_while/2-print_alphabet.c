6b6555c3f39c2266b0f201eeb572211b4c3ac777
hazemghaly <hazemghaly30@gmail.com>
2023-03-19 18:28:16 -0700

    Task 02

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -11,7 +11,7 @@ int main(void)
 char alphabet;
 for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
 {
-/* print the value of the variable*/ 
+/* print the value of the variable*/
 putchar(alphabet);
 }
 putchar('\n');
