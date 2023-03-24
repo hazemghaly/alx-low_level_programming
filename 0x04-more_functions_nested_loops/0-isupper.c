a01b6fe66e3614f6fe5f16fa1be5a31439a527f1
hazemghaly <hazemghaly30@gmail.com>
2023-03-23 19:21:55 -0700

    00

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,14 @@
 #include "main.h"
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
 
 int _isupper(int c)
 
 {
-if(c >= 65  && c <= 90){
+if(c >= '65'  && c <= '90'){
 	
 		return(1);
 	} 
