a65732510864342d94619f24f26546d74ecc4ca2
hazemghaly <hazemghaly30@gmail.com>
2023-03-28 15:46:57 -0700

    04

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_rev - writes the string in revers and a trailing newline to stdout.
+ *@s : first number
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+while (*s != '\0')
+{
+_putchar(*str);
+s = s - 1;
+}
+_putchar('\n');
+}
