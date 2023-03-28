867f0fa77c965b7cf10883af145f293361b13d00
hazemghaly <hazemghaly30@gmail.com>
2023-03-27 22:40:51 -0700

    02

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,14 +1,13 @@
 #include "main.h"
 #include <string.h>
 /**
- * int _strlen -  lrngth of string
- * _strlen(s) - length of strings
+ * _strlen -  lrngth of string
  * @s: first number
  * 
  * Return: 0 (scuess)
  */
 int _strlen(char *s)
 {
-_strlen (s);
+_strlen(s);
 return (0);
 }
