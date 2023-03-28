7e8b6b2318bca5e8b694ce947fec29a18720e185
hazemghaly <hazemghaly30@gmail.com>
2023-03-28 12:58:47 -0700

    03

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- *_puts(char *str) - writes the string s and a trailing newline to stdout.
- *
+ * _puts - writes the string s and a trailing newline to stdout.
+ *@str : first number
  * Return: Always 0.
  */
 void _puts(char *str)
