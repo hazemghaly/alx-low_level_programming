95e2acd99b431be25468bc552cb28ddd656c387d
hazemghaly <hazemghaly30@gmail.com>
2023-03-27 19:49:28 -0700

    00

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * pointer to an int as parameter 
+ * @c: char to check
+ *
+ * Return: 0 
+ */
+
+void reset_to_98(int *n)
+
+{
+ int n;
+int *p; 
+
+n = 98;
+p = &n;  
+return (0);
+}
