842697a3ba620fa71418679a93a2ccdd789b5fb9
hazemghaly <hazemghaly30@gmail.com>
2023-03-29 16:26:24 -0700

    00

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,7 +10,7 @@ char *_strcat(char *dest, char *src)
 {
 int dest_len = strlen(dest);
 int i;
-int n;
+int n = 0;
 for (i = 0 ; i < n && src[i] != '\0' ; i++)
 dest[dest_len + i] = src[i];
 dest[dest_len + i] = '\0';
