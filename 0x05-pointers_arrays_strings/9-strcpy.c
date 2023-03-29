2ef38a55486b1746da1565a847afe13059871bc8
hazemghaly <hazemghaly30@gmail.com>
2023-03-28 19:49:54 -0700

    09

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * *_strcpy - writes the string
+ *@dest : first number
+ *@src : second number
+ * Return: Always 0.
+ */
+char *_strcpy(char *dest, char *src);
+{
+char size_t; 
+int n;
+size_t i;
+for (i = 0; i < n && src[i] != '\0'; i++)
+dest[i] = src[i];
+for ( ; i < n; i++)
+dest[i] = '\0';
+return dest;
+}
