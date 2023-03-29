4e8d72c57a3f7bffd0d4ad8d16a69e578ed1a892
hazemghaly <hazemghaly30@gmail.com>
2023-03-28 17:59:45 -0700

    06

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,14 +10,14 @@ void puts2(char *str)
 int i ;
 int counter = 0;
 while (str[counter] != '\0')
-i = counter % 2
+i = counter % 2;
 if ( i == 0 )
 	{
 _putchar(*str);
 str = str + 1;
 else 
 {
-counter++ 
+counter++;
 }
 }
 _putchar('\n');
