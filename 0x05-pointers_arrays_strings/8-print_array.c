bce7d243b96a9ac387a4b537671aa51bc0cb36bc
hazemghaly <hazemghaly30@gmail.com>
2023-03-28 19:34:57 -0700

    08

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -6,17 +6,16 @@
  *@n : second number
  * Return: Always 0.
  */
-void print_array(int *a, int n)
+void print_array(int *a, int n )
 {
-	int i;
-
-	for (i = 0; i < (n - 1); i++)
-	{
-		printf("%d, ", a[i]);
-	}
-		if (i == (n - 1))
-		{
-			printf("%d", a[n - 1]);
-		}
-			printf("\n");
+int i;
+int t[n];
+a = t;
+for (i = 0; i < (n - 1); i++)
+{
+_putchar(*t[n]);
+_putchar(',');
+_putchar(' ');
+}
+_putchar('\n');
 }
