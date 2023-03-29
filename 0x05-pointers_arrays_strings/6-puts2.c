ef2af729e296e7e02399397a8f9e122d6ce34f31
hazemghaly <hazemghaly30@gmail.com>
2023-03-28 18:12:14 -0700

    06

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * puts2 - writes the string 
+ * puts2 - writes the string
  *@str : first number
  * Return: Always 0.
  */
@@ -9,7 +9,7 @@ void puts2(char *str)
 {
 int counter = 0;
 while (str[counter] != '\0')
-if( counter % 2 == 0 )
+if ( counter % 2 == 0 )
 {
 _putchar(*str);
 str = str + 1;
