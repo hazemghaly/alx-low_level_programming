40d986cbbc29197215d796cbac78bac60d348031
hazemghaly <hazemghaly30@gmail.com>
2023-03-29 21:21:12 -0700

    03

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,8 +13,12 @@
 int _strcmp(char *s1, char *s2)
 {
 int i;
-	for (i = 0 ;  ; i++)
+for (i = 0 ;  ; i++)
 {
+if(s1[i] == '\0' && s2[i] == '\0')
+{
+return(0);
+}
 int a1 = (int) s1[i];
 int a2 = (int) s2[i];
 if(a1 > a2)
