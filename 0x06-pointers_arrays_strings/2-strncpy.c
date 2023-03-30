ddfa272463bdda3f705ca287f860f12b49beefce
hazemghaly <hazemghaly30@gmail.com>
2023-03-29 18:12:35 -0700

    02

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_strncat - overwrites the string n times
+ *@dest : first number
+ *@src : second number
+ *@i : 3rd number
+ *@n : 4th number
+ * Return: Always 0.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int i ;
+for (i = 0 ; i < n && src[i] != '\0' ; i++)
+dest[i] = src[i];
+dest[i] = '\0';
+return(dest);
+}
