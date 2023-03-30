2c584fa000e6e2278e15332769787aa02221a256
hazemghaly <hazemghaly30@gmail.com>
2023-03-29 22:26:31 -0700

    05

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *string_toupper - turn upper 
+ * @s: strimg
+ * @i: the number 
+ * @m: lenght 
+ * Return: nothing.
+ */
+char *string_toupper(char *)
+{
+int i = 0;
+char s;
+int m = strlen(s);
+for ( ; i < m ; i++)
+{
+s[i] = toupper(s[i]);
+}
+}
