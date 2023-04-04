49d7b143518df5bd1cd3e4c2ffb72400821ab0ac
hazemghaly <hazemghaly30@gmail.com>
2023-04-03 18:04:53 -0700

    main.h

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,13 +2,13 @@
 #include "main.h"
 #include <stdio.h>
 
-/** *_memset - fill memory 
- * @s : first char 
- * @b : second char 
- * @n : int 
+/** *_memset - fill memory
+ * @s: first char
+ * @b: second char
+ * @n: int
+ * @i: int
  * return :n sceuss
  */ 
-
 
 char *_memset(char *s, char b, unsigned int n)
 {
@@ -23,8 +23,8 @@ char *_memset(char *s, char b, unsigned int n)
        }
        for (i = 0; s[i] != '\0';)
        {
-                      s[i] = b;
-                }
+	       s[i] = b;
+       }
 i++;
 }
 _putchar('\n');
