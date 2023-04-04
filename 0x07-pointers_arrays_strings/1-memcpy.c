501287b43deb71897a239b053e3010173658019c
hazemghaly <hazemghaly30@gmail.com>
2023-04-03 19:07:50 -0700

    01.

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,20 +2,20 @@
 #include "main.h"
 #include <stdio.h>
 
-/** *_memcpy  - fill memory 
- * @src : first char 
- * @dest : second char 
- * @i: int 
+/** *_memcpy  - fill memory
+ * @src : first char
+ * @dest : second char
+ * @i: int
+ * @n :num of times
  * return :n sceuss
  */
-
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 
    for (int i=0; i<n; i++) 
    {
-       	   dest[n] = src[n]; 
+       	   dest[i] = src[i]; 
 n--;
    }
 return(dest);
