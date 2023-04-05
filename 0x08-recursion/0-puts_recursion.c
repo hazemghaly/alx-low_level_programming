e75743be165726e4502a05ec616060d7294b2f77
hazemghaly <hazemghaly30@gmail.com>
2023-04-05 16:21:46 -0700

    00.0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,16 +1,12 @@
 #include "main.h"
 
 /**
- * _puts - writes the string s and a trailing newline to stdout.
- *@str : first number
+ * _puts_recursion  - writes the string s and a trailing newline 
+ *@s : first number
  * Return: Always 0.
  */
 void _puts_recursion(char *s)
 {
-if(*s = '\0')
-{
-	return(0);
-}
 if(*s != '\0' )
 {
 _putchar(*s);
