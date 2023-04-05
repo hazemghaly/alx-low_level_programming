b7fbd234770d7f21d8cea17c113977d59930c37d
hazemghaly <hazemghaly30@gmail.com>
2023-04-05 16:36:52 -0700

    00.0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,11 +7,13 @@
  */
 void _puts_recursion(char *s)
 {
+       	int  i = 0;
 if (*s != '\0')
 {
 _putchar(*s);
-*s++;
-return(s + 1);
+s++;
+i++;
+return(_puts_recursion(s[i]));
 }
 	_putchar('\n');
 }
