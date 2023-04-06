ceea899c9a165988f4bbfa4a6fa6027151c23a6d
hazemghaly <hazemghaly30@gmail.com>
2023-04-05 17:16:33 -0700

    01.0

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _puts_rev_recursion - writes the string s and a trailing newline
+ * _print_rev_recursion - writes the string s and a trailing newline
  *@s: first number
  * Return: Always 0.
  */
@@ -10,7 +10,7 @@ void _print_rev_recursion(char *s)
 if (*s != '\0')
 {
 _putchar(*s);
-_puts_rev_recursion(s + 1);
+_print_rev_recursion(s + 1);
 }
         _putchar('\n');
 }
