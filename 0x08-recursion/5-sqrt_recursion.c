8e08d2a56ab26ac9886fc56df0d213e0e917578b
hazemghaly <hazemghaly30@gmail.com>
2023-04-05 19:24:07 -0700

    05.0

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <math.h>
+/**
+ *  _sqrt_recursion -  returns the value of x sqrt
+ *@n: first number
+  * Return: Always 0.
+ */
+int _sqrt_recursion(int n);
+{
+if (n < 0 || (n % 2) != 0) 
+{
+return (-1);
+{
+return(_sqrt_recursion(n,0.5)*n);
+}
