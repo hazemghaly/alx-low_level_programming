926d4d7bf27d583278f18130faf33b8a34d0de11
hazemghaly <hazemghaly30@gmail.com>
2023-04-05 17:46:14 -0700

    02.0

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - writes the string s lenght 
+ * _strlen_recursion - writes the string s lenght
  *@s: first number
  * Return: Always 0.
  */
@@ -9,10 +9,10 @@ int _strlen_recursion(char *s)
 {
 if (*s != '\0')
 {
-return(0);
+return (0);
 }
 else
 {
-return(1 + _strlen_recursion(s + 1));
+return (1 + _strlen_recursion(s + 1));
 }
 }
