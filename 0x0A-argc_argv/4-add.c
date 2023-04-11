222c93bdc054908ab45a36d665c4366748d95fa5
hazemghaly <hazemghaly30@gmail.com>
2023-04-11 16:37:57 -0700

    0.04

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,19 +2,18 @@
 #include <stdlib.h>
 /** 
  * main - main function
- *@argv: first char
- *@argc: first int
- *@m: 2nd int
- *@x: 3rd
- *@y: 4th
- *@i: 5th
+ * @argv: first char
+ * @argc: first int
+ * @m: 2nd int
+ * @y: 4th
+ * @i: 5th
  *
- * return: 0 sceuss
+ * return: 0 (sceuss), 1 (Error)
  */
 int main(int argc, char *argv[])
 {
 int i;
-int m; 
+int m;
 if (argc >= 3 && argc != 1 && argc != 2)
 {
 for (i = 1 ; i < argc ; i++)
