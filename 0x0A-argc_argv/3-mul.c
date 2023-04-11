efd055696881c085e71c0c91f20a5f9db2ce3990
hazemghaly <hazemghaly30@gmail.com>
2023-04-11 06:17:23 -0700

    0.3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 #include "main.h"
+#include <stdlib.h>
 /** main - main function
  *@argv: first char
  *@argc: first int
  *@m: 2nd int
+ *@x: 3rd
+ *@y: 4th
  * return :0 sceuss
  */
 int main(int argc, char *argv[])
 {
 int m;
-if (argc >= 3)
+if (argc != 3)
 {
-m = *argv[argc] * *argv[argc + 1];
+int x = atoi(argv[1]);
+int y = atoi(argv[2]);
+m = x * y;
 printf("%d\n", m);
 }
-else
+if (argc == 1 || argc == 2)
 {
 printf("Error\n");
 }
