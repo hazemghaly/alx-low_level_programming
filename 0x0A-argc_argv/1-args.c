471c7b68bf792184aa1f8cf8e6d82c011901ee06
hazemghaly <hazemghaly30@gmail.com>
2023-04-11 04:20:36 -0700

    .1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/** main - main function
+ * @argv: first char
+ * @argc: first int
+ * return :n sceuss
+ */
+int  main(int argc, char *argv[])
+{
+	printf("argc:%d\n", argc );
+return(0);
+}
