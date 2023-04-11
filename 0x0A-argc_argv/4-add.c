5e19aaea6a8e356063bc896ada706b495f260e30
hazemghaly <hazemghaly30@gmail.com>
2023-04-11 07:44:49 -0700

    0.4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,21 +11,18 @@
  */
 int main(int argc, char *argv[])
 {
-int m;
 int i;
-int x = atoi(argv[1]);
-int y ;
-m = x;
+int m; 
 if (argc >= 3 && argc != 1 && argc != 2)
 {
-for (i = 3 ; i <= argc ; i++)
+for (i = 1 ; i < argc ; i++)
 {
-y = atoi(argv[i]);
-m = m + y; 
+int y = atoi(argv[i]);
+m = m + y;
 }
-printf("%d\n", m);
+printf("%d\n",m);
 }
-else if (argc == 1)
+else if(argc == 1)
 {
 printf("0\n");
 }
