5efd36952990351520d86228e2aac71732714dfd
hazemghaly <hazemghaly30@gmail.com>
2023-04-11 05:21:56 -0700

    0.1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,19 +4,21 @@
 /** main - main function
  *@argv: first char
  *@argc: first int
- *@i: int
  * return :n sceuss
  */
 int main(int argc, char *argv[])
 {
-int i;
 if (argc == 0)
 {
-printf("First argument is: %s\n", argv[1]);
+printf("%s\n", argv[0]);
 }
-for (i = 0;i < argc-1;i++)
+if (argc < 2)
 {
-printf("%d\n",i);
+printf("%d\n",argc-1);
+}
+else
+{
+printf ("%d\n",argc-1);
 }
 return (0);
 }
