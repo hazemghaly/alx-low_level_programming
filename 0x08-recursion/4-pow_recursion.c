1ae2e802ce45fc73cf883871ed65ecb25d4c17b8
hazemghaly <hazemghaly30@gmail.com>
2023-06-07 15:40:35 -0700

    power function

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,32 @@
+#include <string.h>
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _pow_recursion - power recursion
+ * @x: int 1st
+ * @y: int 2nd
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _pow_recursion(int x, int y)
+{
+if (y == 0)
+{
+return (1);
+}
+if (y < 0)
+{
+return (-1);
+}
+if (x == 0)
+{
+return (0);
+}
+else
+{
+return (_pow_recursion(x, y + 1) / x );
+}
+return (0);
+}
