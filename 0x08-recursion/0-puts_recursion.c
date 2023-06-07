b1f8ca80d30fe1fd826703c018daaa1023ff5676
hazemghaly <hazemghaly30@gmail.com>
2023-06-07 10:15:40 -0700

    print string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include <string.h>
+#include "main.h"
+#include <stdio.h>
+/** *_memcpy  - fill memory
+ * @src: first char
+ * @dest: second char
+ * @i: int
+ * @n: num of times
+ * @l:lenght
+ * return :n sceuss
+ */
+void _puts_recursion(char *s)
+{
+int i == 0;
+if ( s[i] =! '0\' )
+{
+_putchar('s[i]');
+i++;
+}
+_putchar('');
+return (0);
+}
