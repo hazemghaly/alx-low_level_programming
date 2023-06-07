d8a31086d48c8e176caea61a2f167310634343b0
hazemghaly <hazemghaly30@gmail.com>
2023-06-07 15:23:38 -0700

    print  lenght of string

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,27 @@
+#include <string.h>
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * factorial  - print reverse string with recursion
+ * @n: int 1st
+ *
+ * return :0
+ */
+
+int factorial(int n)
+{
+int 
+if (n < 0)
+{
+return (-1);
+}
+if (n == 0)
+{
+return (1);
+}
+if (n > 0)
+{
+return (n * factorial(n - 1));
+}
+}
