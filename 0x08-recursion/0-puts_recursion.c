59d9c621da9670da126acbf716b3c989672afdf4
hazemghaly <hazemghaly30@gmail.com>
2023-06-07 10:47:38 -0700

    print string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,12 +1,10 @@
 #include <string.h>
 #include "main.h"
 #include <stdio.h>
-/** *_memcpy  - fill memory
- * @src: first char
- * @dest: second char
+/** _puts_recursion  - print string eith recursion
+ * @s: first char
  * @i: int
  * @n: num of times
- * @l:lenght
  * return :n sceuss
  */
 void _puts_recursion(char *s)
