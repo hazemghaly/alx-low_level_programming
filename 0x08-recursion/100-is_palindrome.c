63bd13cd22513591987d4809ee2a903b306f8af0
hazemghaly <hazemghaly30@gmail.com>
2023-06-07 17:52:26 -0700

    square root function

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,30 @@
+#include <string.h>
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * is_palindrome - palindrome recursion
+ * @n: int 1st
+ *
+ * Return: Always 0 (Success)
+ */
+
+int is_palindrome(char *s)
+{
+if (*s != '\0')
+{
+*y = is_palindrome(s + 1);
+}
+if (*s = *y) 
+{
+return (1);
+}
+else if (s == 0)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
