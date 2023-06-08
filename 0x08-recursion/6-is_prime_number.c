eb927f07bd4fc5053655563774b2d858215f3630
hazemghaly <hazemghaly30@gmail.com>
2023-06-07 17:24:58 -0700

    prime  function

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,22 @@
+#include <string.h>
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * is_prime_number - prime number recursion
+ * @n: int 1st
+ *
+ * Return: Always 0 (Success)
+ */
+
+int is_prime_number(int n)
+{
+if ((n % 2) != 0 || (n % 3) != 0 || (n % 5) != 0)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
