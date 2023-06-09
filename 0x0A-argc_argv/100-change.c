7c7a9e6695808c944290fb9ad6750972660a51e2
hazemghaly <hazemghaly30@gmail.com>
2023-06-09 16:55:01 -0700

    coins change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,41 @@
+#include <string.h>
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main  - argument of program
+ * @argc: argument
+ * @argv: argument string
+ * @cents: first int
+ * @y: 2nd int
+ * @i: 3rd int
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main (int argc, char *argv[])
+{
+int i;
+int x;
+int y;
+if (argc >= 3 && argc != 1 && argc != 2)
+{
+for (i = 1 ;i < argc ;i++)
+{
+y = atoi(argv[i]);
+}
+printf("%d\n",y);
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+if (argc == 1)
+{
+printf("0\n");
+}
+return (0);
+}
