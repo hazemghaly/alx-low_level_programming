d52004332dad88914d4f6f097c2336d3ee7fee44
hazemghaly <hazemghaly30@gmail.com>
2023-06-09 13:06:53 -0700

    prog name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <string.h>
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main  - argument of program
+ * @argc: argument
+ * @argv: argument string
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main (int argc, char *argv[])
+{
+printf("argv[0]=%s\n",argv[0]);
+return (0);
+}
