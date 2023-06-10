fb9930443c3de5536006e14a76fa47c9219b983b
hazemghaly <hazemghaly30@gmail.com>
2023-06-09 18:17:32 -0700

    prog name argument

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 /**
  * main  - argument of program
@@ -10,9 +9,6 @@
 int main (int argc, char *argv[])
 {
 printf("%s\n",argv[0]);
-if (argc < 2)
-{
-return (0);
-}
+(void)argc;
 return(0);
 }
