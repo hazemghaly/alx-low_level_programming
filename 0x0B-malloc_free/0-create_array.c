cf274e3fe3aa551dc89d482fe87731ad6163c749
hazemghaly <hazemghaly30@gmail.com>
2023-06-12 16:04:16 -0700

    creates an array of chars, and initializes it

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,11 +11,11 @@
 
 char *create_array(unsigned int size, char c)
 {
-char p;
+char* p;
 if (size  == 0 )
 {
 return (NULL);
 }
-char* p = (int*) malloc(size * sizeof(char));
+p = (int*) malloc(size * sizeof(char));
 return (NULL);
 }
