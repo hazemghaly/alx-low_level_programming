dca076fc739b5783c9c0a1ed191bb21b1cb2fef3
hazemghaly <hazemghaly30@gmail.com>
2023-06-12 15:47:08 -0700

    creates an array of chars, and initializes it

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *create_array  - acreate of array
+ * @c: array
+ * @size: first int
+ * Return: Always 0 (Success) , NULL (Error)
+ */
+
+char *create_array(unsigned int size, char c)
+{
+if (size  == 0 )
+{
+return (NULL);
+}
+c = malloc(size * sizeof(char));
+return (NULL);
+}
