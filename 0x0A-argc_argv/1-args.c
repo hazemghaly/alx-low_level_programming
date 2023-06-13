8c5310e309ded4087e6eabcc94f930f049a28552
hazemghaly <hazemghaly30@gmail.com>
2023-06-12 19:50:49 -0700

    new update of arg

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,19 +9,19 @@
  * Return: Always 0 (Success)
  */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 if (argc == 0)
 {
-printf("%s\n",argv[0]);
+printf("%s\n" , argv[0]);
 }
 if (argc < 2)
 {
-printf("%d\n",argc-1);
+printf("%d\n" , argc - 1);
 }
 else
 {
-printf("%d\n",argc-1);
+printf("%d\n" , argc - 1);
 }
 return (0);
 }
