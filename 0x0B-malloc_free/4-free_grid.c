1285c5cbfaccfeae1091681441e5290cf1712ed8
hazemghaly <hazemghaly30@gmail.com>
2023-06-13 15:33:06 -0700

    free returns a pointer to a 2 dimensional array of integers.

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * free_grid  - free a pointer to a 2 dimensional array of integers.
+ * @grid: 1st int
+ * @height: 2nd int
+ * Return: Always 0 (Success) , NULL (Error)
+ */
+
+void free_grid(int **grid, int height)
+{
+for (int i = 0; i < height; i++)
+{
+free(p[i]);
+}
+free(p);
+}
