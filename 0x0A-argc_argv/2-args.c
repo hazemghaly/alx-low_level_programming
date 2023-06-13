ff0e8559a1bd144e1651737e4dd386a27d8ec166
hazemghaly <hazemghaly30@gmail.com>
2023-06-12 19:52:40 -0700

    new update of  prints all arguments it receives.

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -5,16 +5,16 @@
  * main  - argument of program
  * @argc: argument
  * @argv: argument string
- * @i: first int
+ *
  * Return: Always 0 (Success)
  */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 int i;
 for (i = 0 ; i < argc ; i++)
 {
-printf("%s\n",argv[i]);
+printf("%s\n" , argv[i]);
 }
 return (0);
 }
