3c07554178b008ace53d5b3f015dfc1e48f69522
hazemghaly <hazemghaly30@gmail.com>
2023-06-13 14:54:52 -0700

    concatenates two strings.

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -47,25 +47,14 @@ if (p == NULL)
 {
 return (NULL);
 }
-/*for (i = 0; i < x; i++)
+for (i = 0; i < x; i++)
 {
 p[i] = s2[i];
 }
 for (i = 0; i < y; i++)
 {
-p[len + i] = s1[i];
-}*/
-while(*s1 != '\0')
-{
-p[i] = *s1;
-s1++;
-i++;
+p[len - i] = s1[i];
 }
-while(*s2 != '\0')
-{
-p[i] = *s2;
-s2++;
-i++;
 }
 p[len - 1] = '\0';
 return (p);
