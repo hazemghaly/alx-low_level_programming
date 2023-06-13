3b6c4bc4918b9304cd9fce4d0f04e4578bbfad31
hazemghaly <hazemghaly30@gmail.com>
2023-06-12 19:19:44 -0700

    newly allocated space in memory, which contains a copy of the string given as a parameter.

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -18,7 +18,7 @@ while (*str != '\0')
 len++;
 str++;
 }
-str = (char *) malloc(sizeof(char) * len + 1);
+str = (char *) malloc(sizeof(char) * len );
 if (str == NULL || len == 1)
 {
 return (NULL);
