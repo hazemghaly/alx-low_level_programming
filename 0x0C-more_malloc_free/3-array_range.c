dfeb4dd5fbda081aca7c0e0a3250584565d85758
hazemghaly <hazemghaly30@gmail.com>
2023-06-15 14:23:14 -0700

    function that an array is set

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * *array_range - returns a pointer to a 2 dimensional array of integers.
+ * @min: 1st int
+ * @max: 2nd int
+ * Return: Always 0 (Success) , NULL (Error)
+ */
+
+int *array_range(int min, int max)
+{
+int i;
+int **p;
+if (min <= 0 || max <= 0 || min > max)
+{
+return (NULL);
+}
+p = malloc(max * sizeof(int *));
+if (p == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < max; i++)
+{
+p[i] = malloc(min* sizeof(int));
+if (p[i] == 0)
+{
+while (i--)
+{
+free(p[i]);
+}
+free(p);
+return (NULL);
+}
+}
+return (p);
+}
