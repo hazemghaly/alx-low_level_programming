226e323e5f5571096b32548efa4066b778598fd4
hazemghaly <hazemghaly30@gmail.com>
2023-06-15 16:21:32 -0700

    realloc function

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * *array_range - returns a pointer to a 2 dimensional array of integers.
+ * @*ptr: void
+ * @old_size: 1st int
+ * @new_size: 2nd int
+ * Return: Always 0 (Success) , NULL (Error)
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+unsigned int size, i;
+void *str;
+size = new_size - old_size;
+ptr = malloc(old_size);
+if (ptr == NULL)
+{
+ptr = malloc(new_size);
+exit(ptr);
+}
+if (new_size == 0 && ptr != NULL)
+{
+for (i = 0; i < old_size; i++)
+{
+free(ptr[i]);
+}
+free(ptr);
+exit(NULL);
+}
+else
+{
+str = ptr;
+	if (new_size == old_size)
+	{
+	exit(ptr);
+	}
+str = malloc(size);
+ptr = str + ptr;
+for (i = 0; i < new_size; i++)
+{
+free(ptr[i]);
+}
+free(ptr);
+}
+exit(ptr);
+}
