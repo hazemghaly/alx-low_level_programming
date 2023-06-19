bb1ca4321a1c549afa53ff37bfb8b4faacec7608
hazemghaly <hazemghaly30@gmail.com>
2023-06-19 15:03:31 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
 struct dog
 {
 char *name;
 float age;
-char *owner; 
+char *owner;
 };
+#endif
