dc8a0dd82821c885bde2a9fb3dcd7f52c4013ba7
hazemghaly <hazemghaly30@gmail.com>
2023-06-19 15:16:54 -0700

    init_dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * init_dog - check the code
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Return: Always 0.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+struct dog *d;
+}
