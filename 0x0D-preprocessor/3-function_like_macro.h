92aa641be4bcb47773a7dac1eab77741c5bab664
hazemghaly <hazemghaly30@gmail.com>
2023-06-19 11:29:17 -0700

    Absolute

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#define ABS(x)
+/**
+ * main -  function description
+ *
+ * Return: 0(sucess)
+ */
+int main(void)
+{
+if (n >= 0)
+{
+return (n);
+}
+else
+{
+	return (-n);
+}
+}
