8116e9cea20dbedbd2eccd62cf22d95e8f1069e8
hazemghaly <hazemghaly30@gmail.com>
2023-06-21 16:55:34 -0700

    a function array

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,7 +10,7 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 int i;
-if (size <= 0)
+if (array == NULL || size <= 0 || cmp == NULL)
 {
 return (-1);
 }
@@ -22,9 +22,5 @@ if (array != NULL  || cmp != NULL)
 	}
 return (array[1]);
 }
-else
-{
 return (-1);
-}
-return (0);
 }
