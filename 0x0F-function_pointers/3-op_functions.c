cf9dce8e7f565f36f6413d2cfe98762e201c5ca8
hazemghaly <hazemghaly30@gmail.com>
2023-06-21 17:16:14 -0700

    op_functions

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * op_add - add
+ * op_sub - sub
+ * op_mul - mul
+ * op_div - div   
+ * op_mod - mod
+ * @a: first int
+ * @b: 2nd int
+ * Return: Nothing.
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+int op_div(int a, int b)
+{
+return (a / b);
+}
+int op_mod(int a, int b)
+{
+return (a % b);
+}
