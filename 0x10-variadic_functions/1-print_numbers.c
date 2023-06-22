667e95b3b7993c4c56bc1916c750c0ce0eefd586
hazemghaly <hazemghaly30@gmail.com>
2023-06-22 07:16:16 -0700

    function that prints all its parameters.

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <string.h>
+/**
+ * print_numbers - print of args
+ * @n: int
+ * @separator: char
+ * Return: Always 0.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+va_list args;
+unsigned int i, len;
+va_start (args, separator);
+if (separator == 0)
+{
+exit(0);
+}
+len = strlen(separator);
+for (i = 0; i < len; i++)
+{
+x = va_arg (args, int);
+printf("%d", x);
+}
+printf("\n");
+va_end (args);
+}
