ebe230dc585f07c5db799b983f662cd908439770
hazemghaly <hazemghaly30@gmail.com>
2023-06-22 07:31:17 -0700

    function that prints string all its parameters.

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ * print_strings - print of args
+ * @n: int
+ * @separator: char
+ * Return: Always 0.
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+va_list args;
+unsigned int i, x;
+va_start(args, n);
+if (separator == NULL)
+{
+exit(0);
+}
+for (i = 0; i < n; i++)
+{
+if (separator[i] != NULL)
+{
+x = va_arg(args, int);
+printf("%d", x);
+}
+else
+{
+printf("nil");
+}
+}
+printf("\n");
+va_end(args);
+}
