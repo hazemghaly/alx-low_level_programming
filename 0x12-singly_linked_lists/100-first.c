2f82bbe70409ffa4f56a2711d9cb418f91c96eca
hazemghaly <hazemghaly30@gmail.com>
2023-06-30 16:33:38 -0700

    single lists: tortoise, having pretty good sense of a hare's nature list

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+void _tortoise(void) __attribute__ ((tortoise));
+/** 
+ * tortoise- fexe before main
+ *
+ * Return: Always 0 (Success) , NULL (Error)
+ */
+void _tortoise(void)
+{
+printf(" You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
