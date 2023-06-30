0d57bf7d87caa3152051de4a7eb34fffe96ee037
hazemghaly <hazemghaly30@gmail.com>
2023-06-30 16:22:03 -0700

    single lists: free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/** 
+ * free_list- free a linked
+ * @head: 1st member
+ *
+ * Return: Always 0 (Success) , NULL (Error)
+ */
+void free_list(list_t *head)
+{
+list_t *nodeshead, *node;
+if (!head)
+{
+return (NULL);
+}
+node = head
+while (node)
+{
+nodeshead = node->next;
+free(nodeshead);
+free(node);
+node = nodeshead;
+}
+}
