f89b09a52923594bd1f54b5d1f03b9ba6cf44d23
hazemghaly <hazemghaly30@gmail.com>
2023-06-30 16:47:00 -0700

    single lists: print  list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,20 +1,5 @@
 #include "lists.h"
 /**
- * _strlen -  lrngth of string
- * @s: first number
- * Return: 0 (scuess)
- */
-int _strlen(char *s)
-{
-int longi = 0;
-while (*s != '\0')
-{
-longi++;
-s++;
-}
-return (longi);
-}
-/**
  * print_list- singly linked list
  * @h: 1st member
  *
@@ -23,7 +8,7 @@ return (longi);
 size_t print_list(const list_t *h)
 {
 size_t i = 0;
-int len = _strlen(h->str);
+int len = strlen(h->str);
 if (h->str == NULL)
 {
 printf("[0] (nil)");
