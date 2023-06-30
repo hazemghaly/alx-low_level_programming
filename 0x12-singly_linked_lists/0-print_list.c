819a2fd4bb8a5c0680592c4106acbb23b6de8aee
hazemghaly <hazemghaly30@gmail.com>
2023-06-30 15:26:34 -0700

    single lists:print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,2 +1,23 @@
 #include "lists.h"
-
+/**
+ * print_list- singly linked list
+ * @h: 1st member
+ *
+ * Description: singly linked list node structure
+ */
+size_t print_list(const list_t *h)
+{
+size_t i = 0;
+len = strlen(h->str);
+if (len == 0)
+{
+printf("[0] (nil)");
+}
+while(h)
+{
+printf("[%d] %s\n", len, h->str);
+h = h->next;
+i++;
+}
+return (i);
+}
