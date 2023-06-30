38da3bdfa41956796a61083993410f43d8c5de3e
hazemghaly <hazemghaly30@gmail.com>
2023-06-30 16:06:41 -0700

    single lists: add list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -8,7 +8,7 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-list_t *nodeshead = malloc(sizeof(lists_t));
+list_t *nodeshead = malloc(sizeof(list_t));
 if (!head || !nodeshead)
 {
 return (NULL);
