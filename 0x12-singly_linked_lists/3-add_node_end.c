0e7526f1b9adfedb512d24d4ea321fad460d71f5
hazemghaly <hazemghaly30@gmail.com>
2023-06-30 16:13:42 -0700

    single lists: add at end list

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+/** 
+ * add_node_end- add  in a linked
+ * @head: 1st member
+ * @str: char
+ *
+ * Return: Always 0 (Success) , NULL (Error)
+ */
+list_t *add_node_end(list_t **head, const char *str);
+{
+list_t *nodeshead = malloc(sizeof(lists_t));
+list_t *node = *head;
+if (!head || !nodeshead)
+{
+return (NULL);
+}
+if (str != NULL)
+{
+nodeshead->str = strdup(str);
+if (nodeshead->str == NULL)
+{
+free(nodeshead);
+return (NULL);
+}
+nodeshead->len = _strlen(nodeshead->str);
+}
+if (node)
+{
+while (node->next)
+{
+node = node->next;
+node->next = nodeshead;
+}
+else
+{
+*head = nodeshead;
+return (nodeshead);
+}
+}
