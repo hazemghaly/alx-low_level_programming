5495d87e230da84cb301e5bb4a22221d367bc54e
hazemghaly <hazemghaly30@gmail.com>
2023-07-06 13:51:33 -0700

    a function that converts a binary number

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * binary_to_uint- a function that converts a binary number 
+ * @b: pointer
+ *
+ * Return: Always 0 (Success) , NULL (Error)
+ */
+unsigned int binary_to_uint(const char *b)
+{
+unsigned int a = 1, i = 0, len, j;
+
+unsigned int len;
+len = strlen(b);
+if (b == NULL)
+	return 0;
+for (j = len-1; j >= 0; j--)
+{
+if (b[j] != '0' && b[j] != '1')
+return (0);
+if (b[j] == '1')
+{
+i += a;
+}
+a *= 2;
+}
+return (i);
+}
