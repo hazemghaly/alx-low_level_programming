23747858492a3beec8a24e6c46b84103c549e982
hazemghaly <hazemghaly30@gmail.com>
2023-07-06 16:38:18 -0700

    a function  flip to get from one number to another.

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * SETSBIT -  conut sets 
+ * @n: the number
+ * 
+ * Return: value of the bit
+ */
+int SETSBIT(int j)
+{
+int i = 0;
+while (j > 0) {
+i++;
+j &= (j - 1);
+}
+return (i);
+}
+/**
+ * flip_bits -  filp conut sets 
+ * @n: the number
+ * @m: the number
+ * Return: value of the bit
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+return (SETSBIT(n ^ m));
+}
