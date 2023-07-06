db4e9fd58fd16a2a7f4a9a189ad2641f5de3c020
hazemghaly <hazemghaly30@gmail.com>
2023-07-06 16:15:00 -0700

    a function clear  value of a bit to 1 for a given index.

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * clear_bit -  sets the value of a bit to 1 at a given index.
+ * @n: the number
+ * @index: the index of the bit we want to print
+ * Return: value of the bit
+ */
+int clear_bit(unsigned long int *n, unsigned int index);
+{
+unsigned long int bit = *n;
+if (index > 63)
+{
+return (-1);
+}
+bit = (~(1 << index) & bit);
+return (1);
+}
