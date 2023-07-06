1a7171a6d2154abe647a6ea8fddecfa0ec294955
hazemghaly <hazemghaly30@gmail.com>
2023-07-06 16:49:46 -0700

    endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * get_endianness -  endianness 
+ *
+ * Return: value of the bit
+ */
+int get_endianness(void)
+{
+unsigned int i = 1;
+char *c;
+c = (char *) &i; 
+if (*c == 1) 
+{
+return (0);
+}
+if (*c != 1) 
+{
+return (1);
+}
+}
