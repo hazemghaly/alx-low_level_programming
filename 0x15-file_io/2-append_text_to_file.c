b3afaf3340f55db12bfac58468c226a8d3efaa12
hazemghaly <hazemghaly30@gmail.com>
2023-07-10 15:12:36 -0700

    append file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * append_text_to_file -  append text file
+ * @filename: file name
+ * @text_content: content to be printed
+ * Return: 0
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+int file;
+ssize_t byt;
+ssize_t len = strlen(text_content);
+if (!filename)
+{
+return (-1);
+}
+file = open(filename, "a+");
+if (file == -1)
+{
+return (-1);
+}
+if (len)
+{
+byt = write(file, text_content, len);
+}
+close(file);
+return (byt == len ? 1 : -1);
+}
