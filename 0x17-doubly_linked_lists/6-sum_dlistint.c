186444bc12f22e118294fbd2f3141c1099ba5b75
hazemghaly <hazemghaly30@gmail.com>
2023-08-23 00:45:06 +0000

    double linked sum element

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * sum_dlistint- sum all nth
+ * @head: 1st member
+ *
+ * Return: Always 0 (Success) , NULL (Error)
+ */
+int sum_dlistint(dlistint_t *head)
+{
+size_t ele = 0;
+size_t x = 0;
+while (head)
+{
+ele++;
+x += head;
+head = head->next;
+}
+return (ele);
+}
