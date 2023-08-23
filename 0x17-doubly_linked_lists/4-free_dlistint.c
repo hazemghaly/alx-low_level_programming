12f85364b2c7ed515acbadef045ac3be01159407
hazemghaly <hazemghaly30@gmail.com>
2023-08-23 00:11:10 +0000

    double linked add at end of  all element

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * free_dlistint- free a linked
+ * @head: 1st member
+ *
+ * Return: Always 0 (Success) , NULL (Error)
+ */
+void free_dlistint(dlistint_t *head)
+{
+dlistint *nodeshead, *node;
+if (!head)
+{
+return (0);
+}
+node = head;
+while (node)
+{
+nodeshead = node->next;
+free(node->str);
+free(node);
+node = nodeshead;
+}
