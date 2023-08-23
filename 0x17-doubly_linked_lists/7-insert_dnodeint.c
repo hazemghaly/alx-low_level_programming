36ec91e62ce93f0b8bafa71db7d88ea7e4b9058b
hazemghaly <hazemghaly30@gmail.com>
2023-08-23 01:23:47 +0000

    double linked indexed insert element

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,7 +11,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 unsigned int count = 0;
 dlistint_t *indexadd = malloc(sizeof(dlistint_t));
-dlistint_t *node;
 if (!h || !indexadd)
 {
 	free(indexadd);
@@ -19,37 +18,12 @@ if (!h || !indexadd)
 }
 while (h)
 {
-	if (count == idx)
-	{
-		indexadd->n = n;
-		*h = indexadd;
-		if (indexadd->next == NULL)
-			{
-				indexadd->prev = NULL;
-			}
-			node = *h;
-			if (node)
-			{
-				while (node->next)
-				{
-					node = node->next;
-
-				}
-			node->next = indexadd;
-			indexadd->prev = node;
-			}
-		if (indexadd->prev == NULL)
-			{
-				indexadd->next = NULL;
-			}
-			else
-			{
-				indexadd->next = *h;
-				(*h)->prev = indexadd;
-				*h = indexadd;
-			}
-	}
-	count++;
+if (count == idx)
+{
+indexadd->n = n;
+*h = indexadd;
+}
+count++;
 }
 return (indexadd);
 }
