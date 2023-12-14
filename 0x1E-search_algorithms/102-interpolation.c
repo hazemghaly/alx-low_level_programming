2d64f150b1a14a48a95453300ed24d0211fce7f3
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2023-12-14 16:05:33 +0200

    Update 102-interpolation.c

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * interpolation_search - Entry point
+ * jump_search - Entry point
  * @array: pointer
  * @size:size of array
  * @value:searching value
@@ -11,31 +11,44 @@
 int interpolation_search(int *array, size_t size, int value)
 {
 size_t high, low, i, pos = 0;
-high = 0;
+high = size - 1;
 low = 0;
 if (!array || size == 0)
 {
 	return (-1);
 }
 array[low] = array[0];
-array[high] = array[sizeof(array) - 1];
-printf("%d", array[size]);
+array[high] = array[size - 1];
 pos = low + (
 	((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
+
 for (i = 0; i < size; i++)
 {
-	printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-if (array[i] == array[pos])
+if (pos < size)
+printf("Value checked array[%ld] = [%d]\n", pos, array[pos]);
+else
+{
+printf("Value checked array[%ld] is out of range\n", pos);
+break;
+}
+if (low == high){
+	break;}
+if (array[pos] == value)
 {
 	return (pos);
 }
-if (array[i] > array[pos])
+if (array[pos] < value)
 {
-array[low] = array[0];
-array[high] = array[i];
-pos = low + (
-	((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
-return (pos);
+	low = pos + 1;
+}
+else
+{
+	high = pos - 1;
+		array[low] = array[0];
+	array[high] = array[pos];
+	pos = low + (
+		((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
+	return (pos);
 }
 }
 	return (-1);
